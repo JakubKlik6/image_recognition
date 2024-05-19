f81378176629056b15c3e7c587231e30de6f4d09
Jakub <Jakub.klik6@gmail.com>
2024-05-19 12:28:54 +0200

    -models update

--- ai_web/admin.py
+++ ai_web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Article
+from .models import ImageElement
+from .models import TextElement
 
-# Register your models here.
-admin.site.register(Article)
+admin.site.register(ImageElement)
+admin.site.register(TextElement)
